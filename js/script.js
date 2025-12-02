5a102bf378df92fd56f925198fbbcb29c362006e
saugatksh <saugatb.c21@gmail.com>
2025-12-02 17:55:50 +0545

    first push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Simple message for form submission
+function submitForm() {
+    alert("Thank you! Your message has been sent.");
+}
+
+function toggleMenu() {
+    const menu = document.querySelector('nav ul');
+    menu.classList.toggle('active');
+}
+
